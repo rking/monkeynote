c71c3ae003372526dfdca41742701dcb426b0751
☈king <rking@sharpsaw.org>
2012-12-03 04:13:28 +0000

    Raw initial import of 'rails new'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monkeynote::Application.config.secret_token = '6b4baf36621dab403678867ddf09dcf616765b969e0fc08e19f58782ede6c220acaf0e94e2a4b0dc5f72e1cb3be18a8a2eee454eff8e8097a32116d744568eb8'
